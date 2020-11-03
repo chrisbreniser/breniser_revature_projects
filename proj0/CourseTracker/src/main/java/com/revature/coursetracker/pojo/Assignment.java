8c61895a73e44652b9eaa4c1ac54549165b8dc9f
Chris Breniser <chrisbreniser@gmail.com>
2020-11-03 07:48:49 -0800

    changed file names to create clearer file structure

---
+++ proj0/CourseTracker/src/main/java/com/revature/coursetracker/pojo/Assignment.java
@@ -0,0 +1,106 @@
+package com.revature.coursetracker.pojo;
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+public class Assignment {
+	
+	private UUID id;
+	
+	private String title;
+	
+	private LocalDate startDate;
+	
+	private LocalDate dueDate;
+
+	public Assignment() {
+		super();
+		this.id = UUID.randomUUID();
+	}
+
+	public Assignment(String title, LocalDate startDate, LocalDate dueDate) {
+		this();
+		this.title = title;
+		this.startDate = startDate;
+		this.dueDate = dueDate;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dueDate == null) ? 0 : dueDate.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Assignment other = (Assignment) obj;
+		if (dueDate == null) {
+			if (other.dueDate != null)
+				return false;
+		} else if (!dueDate.equals(other.dueDate))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Assignment [id=" + id + ", title=" + title + ", startDate=" + startDate + ", dueDate=" + dueDate + "]";
+	}
+	
+	
+	
+}
