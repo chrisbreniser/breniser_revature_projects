ebc276ef114889e27ecf4212db56fa355d3cf954
Chris Breniser <chrisbreniser@gmail.com>
2020-11-08 19:15:27 -0800

    Added content to remaining pojos assignment and student

--- proj0/CourseTracker/src/main/java/com/revature/coursetracker/pojo/Assignment.java
+++ proj0/CourseTracker/src/main/java/com/revature/coursetracker/pojo/Assignment.java
@@ -5,5 +5,101 @@ import java.util.UUID;
 
 public class Assignment {
 	
+	private String title;
+	
+	private String description;
+	
+	private String startDate;
+	
+	private String dueDate;
+
+	public Assignment(String title, String description, String startDate, String dueDate) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.startDate = startDate;
+		this.dueDate = dueDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Assignment [title=" + title + ", description=" + description + ", startDate=" + startDate + ", dueDate="
+				+ dueDate + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((dueDate == null) ? 0 : dueDate.hashCode());
+		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Assignment other = (Assignment) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (dueDate == null) {
+			if (other.dueDate != null)
+				return false;
+		} else if (!dueDate.equals(other.dueDate))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
 	
 }
--- proj0/CourseTracker/src/main/java/com/revature/coursetracker/pojo/Student.java
+++ proj0/CourseTracker/src/main/java/com/revature/coursetracker/pojo/Student.java
@@ -3,8 +3,85 @@ package com.revature.coursetracker.pojo;
 public class Student {
 	
 	private String firstName;
+	
 	private String lastName;
+	
 	private String letterGrade;
+
+	public Student(String firstName, String lastName, String letterGrade) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.letterGrade = letterGrade;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLetterGrade() {
+		return letterGrade;
+	}
+
+	public void setLetterGrade(String letterGrade) {
+		this.letterGrade = letterGrade;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + ", letterGrade=" + letterGrade + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((letterGrade == null) ? 0 : letterGrade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (letterGrade == null) {
+			if (other.letterGrade != null)
+				return false;
+		} else if (!letterGrade.equals(other.letterGrade))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
 	
